ef4b7219ec4a99a403d9a6e6b225694060cd2dc3
Shuyang Ji <sji15@illinois.edu>
2022-05-01 19:31:50 -0500

    finalize results and parsing

--- README.md
+++ README.md
@@ -7,14 +7,13 @@ By default, TiKV uses three replicas to form a Raft Group
 
 1. Install TiUP: `curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh`
 2. Run `tiup playground --pd 1 --kv 3 --without-monitor` to start a 1-node PD and 3-node TiKV cluster. (Find out each node's pid with `tiup playground display`)
-3. Check  `~/.tiup/data/` and update the `experiement_folder` variable at the top of `benchmark.py`
-4. Setup go-ycsb
+3. Setup go-ycsb
     ```
     git clone https://github.com/pingcap/go-ycsb.git
     cd go-ycsb
     make
-    ./bin/go-ycsb
     ```
+4. run `benchmark.xonsh` (comment out parts of main to run some tests seperately)
 <!--
 ## References
 * https://github.com/pingcap/tiup/blob/master/doc/user/overview.md
--- log_to_json.py
+++ log_to_json.py
@@ -1,69 +1,55 @@
 
    
+from collections import defaultdict
 import glob
 import os
 import json
 
-read_map = {}
-update_map = {}
+read_map = defaultdict(list)
+update_map = defaultdict(list)
+sort_metrics_map = {"baseline": "thread", "slowcpu": "quota", "memcontention": "quota"}
+no_fault_map = defaultdict(list)
 ycsb_files = glob.glob("logs/*")
+final_thread_num = 26
 for ycsb_file in ycsb_files:
     file_name = os.path.basename(ycsb_file)
     tokens = file_name.split("_")
-    thread_num = tokens[2]
-    # print(tokens)
-    # fault_type = tokens[-2] + "_" + tokens[-1]
-    # print(fault_type)
-    perf = {"thread": 0,"throughput": 0.0, "latency": 0.0}
-    if tokens[0] == "baseline":
-        fault_type = "baseline"
-    if fault_type not in read_map:
-        read_map[fault_type] = []
-    if fault_type not in update_map:
-        update_map[fault_type] = []
+    perf = {"throughput": 0.0, "latency": 0.0}
+    if tokens[0] in ["baseline"]:
+        fault_type = tokens[0]
+        thread_num = tokens[2]
+    elif tokens[0] in ["slowcpu"]:
+        fault_type = f"{tokens[0]}_{tokens[1]}_{tokens[3]}"
+    elif tokens[0] in ["memcontention"]:
+        fault_type = f"{tokens[0]}_{tokens[1]}_{tokens[2]}"
+    elif tokens[0] in ["crash"]:
+        fault_type = f"{tokens[0]}_{tokens[1]}"
+    else:
+        fault_type = "nofault"
     for line in open(ycsb_file).readlines():
-        # READ   - Takes(s): 0.3, Count: 10000, OPS: 28679.4, Avg(us): 372, Min(us): 72, Max(us): 32742, 99th(us): 2000, 99.9th(us): 4000, 99.99th(us): 33000
         if "READ   - " in line:
             throughput = float(line.split("Avg(us): ")[1].split(",")[0])
             latency = float(line.split("OPS: ")[1].split(",")[0])
+            if fault_type == 'baseline':
+                perf["thread"] = int(thread_num)
             perf["throughput"] = throughput
             perf["latency"] = latency
-            perf["thread"] = thread_num
-            read_map[fault_type].append(perf)
+            read_map[fault_type].append(perf.copy())
         elif "UPDATE - " in line:
             throughput = float(line.split("Avg(us): ")[1].split(",")[0])
             latency = float(line.split("OPS: ")[1].split(",")[0])
             perf["throughput"] = throughput
             perf["latency"] = latency
-            perf["thread"] = int(thread_num)
-            update_map[fault_type].append(perf)
-
-
-        # UPDATE - Takes(s): 0.9, Count: 9966, OPS: 10780.4, Avg(us): 2930, Min(us): 319, Max(us): 99317, 99th(us): 10000, 99.9th(us): 36000, 99.99th(us): 100000
-        # elif "UPDATE - " in line:
-        #     perf["latency"] = float(line.strip().split(" ")[-1])
-        # elif "[UPDATE], AverageLatency(us)," in line:
-        #     perf["latency"] = float(line.strip().split(" ")[-1])
+            if fault_type == 'baseline':
+                perf["thread"] = int(thread_num)
+                continue # only the last line for baseline
+            update_map[fault_type].append(perf.copy())
+    if fault_type == 'baseline':
+        update_map[fault_type].append(perf.copy())
 print(read_map)
-#     if fault_type not in map:
-#         map[fault_type] = []
-#     perf = {"throughput": 0.0, "read_latency": 0.0, "update_latency": 0.0}
-#     for line in open(ycsb_file).readlines():
-#         if "[OVERALL], Throughput(ops/sec)," in line:
-#             perf["throughput"] = float(line.strip().split(" ")[-1])
-#         elif "[READ], AverageLatency(us)," in line:
-#             perf["read_latency"] = float(line.strip().split(" ")[-1])
-#         elif "[UPDATE], AverageLatency(us)," in line:
-#             perf["update_latency"] = float(line.strip().split(" ")[-1])
-#     map[fault_type].append(perf)
 
 for fault_type in update_map:
-    update_map[fault_type] = sorted(update_map[fault_type], key=lambda i: i["thread"])
-print(update_map)
-json.dump(update_map, open("result.json", "w"), indent=4)
+    if fault_type == 'baseline':
+        update_map[fault_type] = sorted(update_map[fault_type], key=lambda i: i["thread"])
 
-# for fault_type in map:
-#     str = fault_type
-#     for item in map[fault_type]:
-#         str += "\t({},{})".format(int(item["throughput"]), int(item["update_latency"]))
-#     print(str)
+json.dump(update_map, open("result.json", "w"), indent=4)
